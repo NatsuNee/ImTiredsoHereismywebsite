eb02977ddaa7e01176ff766fc26ec5a0bbfdad1e
Stephen Thach <69834994+NatsuNee@users.noreply.github.com>
2023-10-28 04:22:45 -0700

    2.0 regressive

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-var area = 1;
-
-const loginscr = document.getElementById('login');
-const desktopscr = document.getElementById('desktop');
-
-function hideAll(){
-    
-    
-}
-
-function setArea(){
-    if (area == 0){
-        desktopscr.style.display="none";
-        loginscr.style.display = "block";
-    }
-    else if (area == 1){
-        loginscr.style.display="none";
-        desktopscr.style.display = "block";
-    }
-    else {
-        hideAll();
-    }
-}
-
-function setlogin(){
-    area = 0;
-    setArea();
-}
-
-function setmain(){
-    area = 1;
-    setArea();
-}
-
 let time = document.getElementById("current-time");
 
 setInterval(() =>{
