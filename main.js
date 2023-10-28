ad811a7f0269508f23cc125c9fd30601c60a855d
Stephen Thach <69834994+NatsuNee@users.noreply.github.com>
2023-10-28 04:18:40 -0700

    2.0 Regressive

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-import * as THREE from 'three';
+var area = 1;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loginscr = document.getElementById('login');
+const desktopscr = document.getElementById('desktop');
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+function hideAll(){
+    
+    
 }
 
-animate();
+function setArea(){
+    if (area == 0){
+        desktopscr.style.display="none";
+        loginscr.style.display = "block";
+    }
+    else if (area == 1){
+        loginscr.style.display="none";
+        desktopscr.style.display = "block";
+    }
+    else {
+        hideAll();
+    }
+}
 
+function setlogin(){
+    area = 0;
+    setArea();
+}
 
+function setmain(){
+    area = 1;
+    setArea();
+}
 
 let time = document.getElementById("current-time");
 
